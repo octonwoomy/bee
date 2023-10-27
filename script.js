db01e90de70d75883576944062d9b399532d6626
octonwoomy <93731973+octonwoomy@users.noreply.github.com>
2023-10-26 21:51:39 -0500

    done lol

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const buttons = document.querySelectorAll('.dropdown-button');
 
+buttons.forEach(button => {
+    button.addEventListener('click', function () {
+        this.classList.toggle('active');
+    });
+});
